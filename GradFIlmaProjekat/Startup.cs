69c95f91788da7e7410536bcd64941e5c9b881b8
sumka <sbotulja1@etf.unsa.ba>
2015-11-16 21:39:28 +0100

    Pocetne postavke

---
+++ GradFIlmaProjekat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GradFIlmaProjekat.Startup))]
+namespace GradFIlmaProjekat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
