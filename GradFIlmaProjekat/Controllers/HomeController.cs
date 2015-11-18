c9d3cd0eb885a60de872804c7bec450478e60dcf
sumka <sbotulja1@etf.unsa.ba>
2015-11-18 01:39:24 +0100

    login i mail

--- GradFIlmaProjekat/Controllers/HomeController.cs
+++ GradFIlmaProjekat/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
+using System.IO;
+using GradFIlmaProjekat.Models;
 
 namespace GradFIlmaProjekat.Controllers
 {
@@ -23,8 +26,35 @@ namespace GradFIlmaProjekat.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
-            return View();
+            var model = new Mail();
+            return View(model);
         }
+
+        [HttpPost]
+        public ActionResult Contact(Mail model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                MailMessage m = new MailMessage("aidagranulo@gmail.com", "aidagranulo@gmail.com");
+              // m.Subject=model.Subject;
+               string poruka = "Ime: " + model.Name + " Email: " + model.Email + " Telefon: " + model.Phone + " Poruka: " + model.Message; 
+                m.Body = poruka;
+               m.IsBodyHtml = true;
+               SmtpClient s = new SmtpClient("smtp.gmail.com",587);
+                s.Credentials = new System.Net.NetworkCredential()
+                { UserName="aidagranulo@gmail.com",
+                 Password="subhanAllah"
+                };
+
+                s.EnableSsl=true;
+                s.Send(m);
+                ViewBag.Message = "Email poslan!";
+                Response.Write("Email poslan!");
+               
+            }
+        return View(model);
+        }
+             
     }
 }
---
+++ GradFIlmaProjekat/Models/Mail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GradFIlmaProjekat.Models
+{
+    public class Mail
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Message { get; set; }
+    }
+}
